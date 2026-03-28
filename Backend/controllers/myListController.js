d03aedb1f6a8461c378841f6b016725cec8d9a41
sachindataninja123 <sachinkumarkushwaha991071@gmail.com>
2026-03-29 01:18:05 +0530

    My list Routes Completed

--- Backend/controllers/myListController.js
+++ Backend/controllers/myListController.js
@@ -54,4 +54,62 @@ const addToMyList = async (req, res) => {
   }
 };
 
-module.exports = { addToMyList };
+const deleteToMyList = async (req, res) => {
+  try {
+    const myListItem = await myListModel.findById(req.params.id);
+
+    if (!myListItem) {
+      return res.status(404).json({
+        message: "The Item id was not Found!",
+        error: true,
+        success: false,
+      });
+    }
+    const deletedItem = await myListModel.findByIdAndDelete(req.params.id);
+
+    if (!deletedItem) {
+      return res.status(404).json({
+        message: "The Item is not deleted!",
+        error: true,
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Item removed from My List",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      success: false,
+      error: true,
+    });
+  }
+};
+
+const getAllMyList = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const myListItem = await myListModel.find({
+        userId : userId
+    })
+
+    return res.status(200).json({
+        success : true,
+        data : myListItem,
+        error: false,
+    })
+
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      success: false,
+      error: true,
+    });
+  }
+};
+
+module.exports = { addToMyList, deleteToMyList  , getAllMyList};
--- Backend/routes/myListRoutes.js
+++ Backend/routes/myListRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const auth = require("../middlewares/auth");
-const { addToMyList } = require("../controllers/myListController");
+const {
+  addToMyList,
+  deleteToMyList,
+  getAllMyList,
+} = require("../controllers/myListController");
 
 const myListRouter = express.Router();
 
 myListRouter.post("/add", auth, addToMyList);
+myListRouter.get("/", auth, getAllMyList);
+myListRouter.delete("/:id", auth, deleteToMyList);
 
-
-module.exports = myListRouter
+module.exports = myListRouter;
